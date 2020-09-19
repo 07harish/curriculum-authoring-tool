70199d3149b77c974a39b4746130ee0b35f962c5
Harish Kulkarni <07harish96@gmail.com>
2020-09-20 01:07:28 +0530

    added usage details

--- src/Components/Usage.js
+++ src/Components/Usage.js
@@ -12,21 +12,21 @@ const Usage = () => {
         ✖
       </div>
       <h1>
-        <center>📘 Curriculum Authoring Tool</center>
+        <center><span aria-label="Emoji" role='img'>📘</span> Curriculum Authoring Tool</center>
       </h1>
       <h4>
         <center>
-          <span role='img'>✨ </span>This can be mainly used to design the
+          <span aria-label="Emoji" role='img'>✨ </span>This can be mainly used to design the
           Syllabus or Curriculum for schools, colleges and elsewhere
         </center>
       </h4>
       <hr></hr>
       <p>
-        <span role='img'>✅ </span>You can create Topics and Subtopics. Add
+        <span aria-label="Emoji" role='img'>✅ </span>You can create Topics and Subtopics. Add
         text, indent, outdent, move and delete rows.
       </p>
       <p>
-        <span role='img'>✅ </span>You can import and export as a json file
+        <span aria-label="Emoji" role='img'>✅ </span>You can import and export as a json file
       </p>
       <ul>
         <li>Indent a row to make it a subtopic</li>
